186a51f1a33cb342e653eaf65996bc0a63c96dd8
2220879 <2220879@my.ipleiria.pt>
2024-12-17 18:20:41 +0000

    Projeto - 17/12/2024

--- produtosginasiosis/backend/modules/api/controllers/FavoritoController.php
+++ produtosginasiosis/backend/modules/api/controllers/FavoritoController.php
@@ -3,6 +3,8 @@
 namespace backend\modules\api\controllers;
 
 use backend\modules\api\components\CustomAuth;
+use common\models\User;
+use Yii;
 use yii\rest\ActiveController;
 
 class FavoritoController extends ActiveController
@@ -11,27 +13,48 @@ class FavoritoController extends ActiveController
 
     public function behaviors()
     {
+        Yii::$app->params['id'] = 0;
         $behaviors = parent::behaviors();
         $behaviors['authenticator'] = [
             'class' => CustomAuth::className(),
         ];
-
         return $behaviors;
     }
 
     public function actionCount()
     {
-        $favoritosmodel = new $this->modelClass;
-        $recs = $favoritosmodel::find()->all();
-        return ['count' => count($recs)];
+        $userID = Yii::$app->params['id'];
+
+        if ($user = User::find()->where(['id' => $userID])->one()) {
+            // Verifica se o utilizador tem o papel "cliente"
+            if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
+                return 'O Utilizador introduzido não tem permissões de cliente';
+            } else {
+                $favoritosmodel = new $this->modelClass;
+                $recs = $favoritosmodel::find()->all();
+                return ['count' => count($recs)];
+            }
+        }
+
+        return 'Não foi possível contar os favoritos.';
 
     }
 
     public function actionFavoritos()
     {
-        $favoritosmodel = new $this->modelClass;
-        $produtos = $favoritosmodel::find()->all();
-        return ['produtos' => $produtos];
+        $userID = Yii::$app->params['id'];
+
+        if ($user = User::find()->where(['id' => $userID])->one()) {
+            // Verifica se o utilizador tem o papel "cliente"
+            if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
+                return 'O Utilizador introduzido não tem permissões de cliente';
+            } else {
+                $favoritosmodel = new $this->modelClass;
+                $produtos = $favoritosmodel::find()->all();
+                return ['produtos' => $produtos];
+            }
+        }
+        return 'Não foi possível obter os favoritos.';
     }
 
 
